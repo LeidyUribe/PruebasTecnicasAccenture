0d31307c52ec808b7dd6260bbb33bbc3c35f0c99
LeidyUribe <leidyu.f@gmail.com>
2025-11-15 07:49:14 -0500

    Primeros cambios y ajustes para la aplicacion base

---
+++ Frontend/todo-list-app/src/main.ts
@@ -0,0 +1,13 @@
+// main.ts - VERSIÓN FINAL CORRECTA
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideIonicAngular({}),
+    provideRouter(routes)
+  ]
+});
